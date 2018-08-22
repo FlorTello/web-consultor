a50fb899442b60d5ccf666da06f405305b3f81f0
Flor de la Cantuta Tello Sarmiento <ftellosa@everis.com>
2018-08-22 11:12:36 -0500

    first commit

---
+++ jest.config.js
@@ -0,0 +1,44 @@
+module.exports = {
+  globals: {
+    '__TS_CONFIG__': {
+      'target': 'es6',
+      'module': 'commonjs',
+      'moduleResolution': 'node'
+    },
+    'ts-jest': {
+      'tsConfigFile': 'src/tsconfig.spec.json'
+    },
+    '__TRANSFORM_HTML__': true
+  },
+  preset: "jest-preset-angular",
+  roots: ['src'],
+  setupTestFrameworkScriptFile: "<rootDir>/src/setup-jest.ts",
+  moduleFileExtensions: [
+    'ts',
+    'tsx',
+    'js',
+    'json'
+  ],
+  testPathIgnorePatterns: [
+    '/node_modules/',
+    '/dist/',
+    'src/app/*.{js}'
+  ],
+  testResultsProcessor: 'jest-sonar-reporter',
+  moduleNameMapper: {
+    "app/(.*)": "<rootDir>/src/app/$1",
+    "@common/(.*)": "<rootDir>/src/app/common/$1",
+  },
+  collectCoverageFrom: [
+    'src/app/module/**/*.{ts}',
+    'src/app/*.{ts}',
+    '!src/app/**/*.{js}',
+    '!src/app/environment/*.{ts}',
+    '!src/app/language/*.{ts}',
+    '!src/app/**/*.module.{ts}',
+    '!src/app/**/*.interface.{ts}',
+    '!src/app/**/*.state.{ts}',
+    '!src/app/**/*.entity.{ts}'
+  ],
+  testResultsProcessor: 'jest-sonar-reporter',
+}
