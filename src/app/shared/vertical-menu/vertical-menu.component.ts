b2f49ef7bc28717772fdd0d7b23f0817f608cab6
Flor de la Cantuta Tello Sarmiento <ftellosa@everis.com>
2018-08-23 15:37:50 -0500

    feat(all): add component menu

--- src/app/shared/vertical-menu/vertical-menu.component.ts
+++ src/app/shared/vertical-menu/vertical-menu.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VerticalMenuComponent implements OnInit {
 
+  expandHeight = '42px';
+  collapseHeight = '42px';
+
   constructor() { }
 
   ngOnInit() {
